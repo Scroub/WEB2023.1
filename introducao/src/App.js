60b6dc5601ab5e15484f41ddda8d1aab933d73de
Scroub <iuryoliqueiroz@gmail.com>
2023-03-13 22:55:44 -0300

    Atualizando

---
+++ introducao/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import HelloWorld from "./Component/HelloWorld";
+
+
+function App() {
+  return (
+    <div>
+      1. <HelloWorld />
+      2. <HelloWorld />
+      3. <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
---
+++ introducao/src/Component/HelloWorld.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+// --- Const Function --- //
+
+// function HelloWorld () {
+//     return (
+//         <div>
+//             <h1>Iury de Oliveira</h1>
+//             <h2>Sistemas de Informação</h2>
+//         </div>
+//     )
+// }
+
+// --- Arrow Function --- //
+
+// const HelloWorld = () => {
+//     return (
+//         <div>
+//             <h1>Iury de Oliveira</h1>
+//             <h2>Sistemas de Informação</h2>
+//         </div>
+//     )
+// }
+
+// --- Arrow Function v2 --- //
+
+// const HelloWorld = () =>
+//     <div>
+//         <h1>Iury de Oliveira</h1>
+//         <h2>Sistemas de Informação</h2>
+//     </div>
+
+// --- V3 com classes --- //
+
+class HelloWorld extends React.Component{
+    render() {
+      return (
+        <div>
+          <h1>Iury de Oliveira</h1>
+          <h2>Sistemas de informação</h2>
+        </div>
+      );
+    }
+  }
+
+export default HelloWorld;
